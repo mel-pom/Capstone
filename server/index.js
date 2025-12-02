5b84f5b68d93c0cdcdb068d19b75923b1c7ace67
mel-pom <melpomerleau@gmail.com>
2025-12-01 18:50:08 -0800

    connect mongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -22,6 +22,6 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
   })
   .catch((err) => console.error("MongoDB connection error:", err));

fd06955fa970f23829f240d43058b65c96e2002b
mel-pom <melpomerleau@gmail.com>
2025-12-05 12:28:35 -0800

    fixed authentication, tested registration/logins

--- server/models/User.js
+++ server/models/User.js
@@ -25,6 +25,10 @@ const userSchema = new mongoose.Schema({
       if (!this.isModified("password")) return;
       this.password = await bcrypt.hash(this.password, 10);
     });
+
+    userSchema.methods.comparePassword = function (candidatePassword) {
+        return bcrypt.compare(candidatePassword, this.password);
+      };
     
     const User = mongoose.model("User", userSchema);
     export default User;
